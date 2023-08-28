f01355bff5f431694345475cee906b0919070c5e
melvincorners1995@gmail.com <melvincorners1995@gmail.com>
2023-08-28 16:38:53 +0000

    Edited Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -20,6 +20,8 @@ class Home(HomeTemplate):
 
     Globals.filters_manually_shown = False
 
+    self.product_info_button.enabled = False
+
   def cancel(self):
     self.content_panel.clear()
     self.content_panel.add_component(Home())
